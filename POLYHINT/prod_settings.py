267db9f73bb4e59edbd1f75381cb13c347f333f0
POD <papaomardiop00@gmail.com>
2020-04-15 22:56:44 +0000

    'aws'

--- POLYHINT/prod_settings.py
+++ POLYHINT/prod_settings.py
@@ -2,6 +2,8 @@ from .settings import *
 import dj_database_url
 import storages
 from storages.backends.s3boto import S3BotoStorage
+import boto
+
 
 
 
@@ -27,13 +29,12 @@ INSTALLED_APPS += ('storages',)
 #MEDIA_URL = 'http://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
 #DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
 
-export AWS_STORAGE_BUCKET_NAME = 'polyhint2'
-export AWS_S3_REGION_NAME = 'us-east-2'  # e.g. us-east-2
-export AWS_ACCESS_KEY_ID = 'AKIAIYKJDOYIV5UATSUA'
-export AWS_SECRET_ACCESS_KEY = 'hP9mmdCJnvbHHoz+JT0I3AqrDNKwdQGxItMxnx1P'
+AWS_STORAGE_BUCKET_NAME = 'polyhint2'
+AWS_S3_REGION_NAME = 'us-east-2'  # e.g. us-east-2
+AWS_ACCESS_KEY_ID = 'AKIAIYKJDOYIV5UATSUA'
+AWS_SECRET_ACCESS_KEY = 'hP9mmdCJnvbHHoz+JT0I3AqrDNKwdQGxItMxnx1P'
 
-# Tell django-storages the domain to use to refer to static files.
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+conn = boto.connect_s3(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
 
 MEDIA_URL = 'http://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
 
