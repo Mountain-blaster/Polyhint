8c05c6b0f903cf866b3abb9a57e1793532277551
POD <papaomardiop00@gmail.com>
2020-04-16 01:59:36 +0000

    'aws'

--- POLYHINT/prod_settings.py
+++ POLYHINT/prod_settings.py
@@ -28,7 +28,6 @@ INSTALLED_APPS += ('storages',)
 #DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
 
 AWS_STORAGE_BUCKET_NAME = 'polyhint2'
-AWS_S3_REGION_NAME = 'us-east-2'  # e.g. us-east-2
 AWS_ACCESS_KEY_ID = 'AKIAIYKJDOYIV5UATSUA'
 AWS_SECRET_ACCESS_KEY = 'hP9mmdCJnvbHHoz+JT0I3AqrDNKwdQGxItMxnx1P'
 
