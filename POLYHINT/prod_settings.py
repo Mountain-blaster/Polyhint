c2bb0e7798868cef13010e63dc5149055b32b8e3
POD <papaomardiop00@gmail.com>
2020-04-16 19:05:50 +0000

    'aws'

--- POLYHINT/prod_settings.py
+++ POLYHINT/prod_settings.py
@@ -1,8 +1,5 @@
 from .settings import *
 import dj_database_url
-import storages
-
-
 
 
 DEBUG = True
@@ -10,22 +7,13 @@ TEMPLATE_DEBUG = False
 
 SECRET_KEY = 'd%5_y%y8&cu2k%1j*k80f$+qd7ix5q$&jkplokgsdo4=o#)3ek'
 
-ALLOWED_HOSTS = ['polyhint.herokuapp.com', 'polyhint2.herokuapp.com']
+ALLOWED_HOSTS = ['polyhint.herokuapp.com']
 
 DATABASES['default'] = dj_database_url.config()
 
 MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
 
 INSTALLED_APPS += ('storages',)
-
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorages'
-
-#AWS_QUERYSTRING_AUTH = False
-#AWS_ACCESS_KEY_ID = ['AKIAIYKJDOYIV5UATSUA']
-#AWS_SECRET_ACCESS_KEY = ['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']
-#AWS_STORAGE_BUCKET_NAME = ['polyhint2']
-#MEDIA_URL = 'http://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
-#DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
 
 AWS_STORAGE_BUCKET_NAME = 'polymedia'
 AWS_S3_REGION_NAME = 'us-west-2'
@@ -35,4 +23,3 @@ AWS_SECRET_ACCESS_KEY = 'hP9mmdCJnvbHHoz+JT0I3AqrDNKwdQGxItMxnx1P'
 MEDIA_URL = 'http://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
-
