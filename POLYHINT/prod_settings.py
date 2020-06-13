cd1a3838f79b7d80be9bde97830d929ecbc18b86
POD <papaomardiop00@gmail.com>
2020-06-13 19:40:09 +0000

    'deployement'

--- POLYHINT/prod_settings.py
+++ POLYHINT/prod_settings.py
@@ -1,12 +1,12 @@
 from .settings import *
 import dj_database_url
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = False
 
 SECRET_KEY = 'd%5_y%y8&cu2k%1j*k80f$+qd7ix5q$&jkplokgsdo4=o#)3ek'
 
-ALLOWED_HOSTS = ['polyhint.herokuapp.com', 'polyhint2.herokuapp.com']
+ALLOWED_HOSTS = ['polyhint.herokuapp.com']
 
 DATABASES['default'] = dj_database_url.config()
 
